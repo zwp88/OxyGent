60f9b3f1ab0a8a2a5eeae2336ff34580e5437650
Alchuang22-dev <2584829494@qq.com>
2025-07-23 02:53:09 -0700

    fix(aioredis): bypass python 3.13 aioredis bug

--- oxygent/databases/db_redis/__init__.py
+++ oxygent/databases/db_redis/__init__.py
@@ -1,5 +1,11 @@
+import sys
+
+if sys.version_info < (3, 11):
+    from .jimdb_ap_redis import JimdbApRedis
+else:
+    JimdbApRedis = None  
+
 from .base_redis import BaseRedis
-from .jimdb_ap_redis import JimdbApRedis
 from .local_redis import LocalRedis
 
 __all__ = ["JimdbApRedis", "BaseRedis", "LocalRedis"]
